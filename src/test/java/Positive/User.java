e5a61227fde48432b4079038a0fddf8f6430025f
Samindya Vass <samindyauv@gmail.com>
2025-04-23 22:29:42 +0530

    Search User Using Name

--- src/test/java/Positive/User.java
+++ src/test/java/Positive/User.java
@@ -16,6 +16,8 @@ public class User extends baseTest {
         loadUrl();
         webSteps.login();
         webSteps.waiting();
+        webSteps.click("ClickUser");
+        webSteps.click("ClickUserList");
     }
     @Test(priority = 1)
     public void addUser() throws InterruptedException, AWTException {
@@ -29,8 +31,6 @@ public class User extends baseTest {
                 "<br>Step 5- Fill Primary Details" +
                 "<br>Step 6- Click 'Save' Button"
                 );
-        webSteps.click("ClickUser");
-        webSteps.click("ClickUserList");
         webSteps.click("ClickAddNewUserButton");
         webSteps.type("Amal Perera","AddUser_Name");
         //webSteps.select("AddUser_Role",5,1);
@@ -41,5 +41,23 @@ public class User extends baseTest {
         webSteps.type("Kuliyapitiya","AddUser_Address");
         webSteps.click("AddUser_SaveButton");
         //Assert.assertEquals("User added successfully",webSteps.getText("AddUser_ToastMessage"), "Passed");
+    }
+
+    @Test(priority = 2)
+    public void searchUserUsingName() throws InterruptedException, AWTException {
+        extentReportManager.startTest("User Functionality", "<b>Search User Using Name</b>");
+        extentReportManager.testSteps("<b><font color='blue'>Test Case : </font>TC01: Verify that the user can search a user using user's name</b>");
+        extentReportManager.testSteps("<b><font color='blue'>Test Steps : </font></b>" +
+                "<br>Step 1- Login to the System" +
+                "<br>Step 2- Click User " +
+                "<br>Step 3- Click User List" +
+                "<br>Step 4- Select User from 'Search By' dropdown " +
+                "<br>Step 5- Type in Search" +
+                "<br>Step 6- Click Search"
+        );
+        webSteps.select("SearchUser_SearchByDropdown",2,0);
+        webSteps.type("Kasun Bandara","SearchUser_SearchBar");
+        webSteps.click("SearchUser_SearchButton");
+        Assert.assertEquals("Kasun Bandara",webSteps.getText("SearchUser_Result"), "Passed");
     }
 }
