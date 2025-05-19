e0b1ee633db111da1469d6be99679f1ee048009f
Samindya Vass <samindyauv@gmail.com>
2025-05-19 13:39:14 +0530

    Search Location

--- src/test/java/Positive/BusinessSettings/Location.java
+++ src/test/java/Positive/BusinessSettings/Location.java
@@ -2,7 +2,9 @@ package Positive.BusinessSettings;
 
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+import utils.PropertyUtils;
 import utils.baseTest;
 import utils.extentReportManager;
 
@@ -43,6 +45,36 @@ public class Location extends baseTest {
         webSteps.click("SaveButton");
         webSteps.implicitWait("ToastMessage");
         Assert.assertEquals("Location created successfully",webSteps.getText("ToastMessage"));
+    }
+
+    @DataProvider(name = "locationSearchData")
+    public Object[][] locationSearchData() {
+        return new Object[][]{
+                {"Location", PropertyUtils.getProperty("Location_Name"), 2},
+                {"City", PropertyUtils.getProperty("Location_City"), 4},
+                {"Email", PropertyUtils.getProperty("Location_Email"), 6},
+                {"Address", PropertyUtils.getProperty("Location_Address"), 3}
+        };
+    }
+
+    @Test(dataProvider = "locationSearchData", priority = 2)
+    public void searchLocation (String criteriaType,String inputValue, int columnIndex) throws InterruptedException, AWTException {
+        extentReportManager.startTest("Cities Functionality", "<b>Search Location Using " + criteriaType + "</b>");
+        extentReportManager.testSteps("<b><font color='blue'>Test Case : </font>Verify that the location can search by " + criteriaType.toLowerCase() + "</b>");
+        extentReportManager.testSteps("<b><font color='blue'>Test Steps : </font></b>" +
+                "<br>Step 1 - Login to the System" +
+                "<br>Step 2- Click Settings " +
+                "<br>Step 3- Click Business Settings " +
+                "<br>Step 4- Click Locations " +
+                "<br>Step 5 - Select '" + criteriaType + "' from 'Search By' dropdown" +
+                "<br>Step 6 - Enter Search Input" +
+                "<br>Step 7 - Click Search"
+        );
+        webSteps.passValue(criteriaType,"SearchBy_Dropdown");
+        webSteps.type(inputValue,"SearchBy_SearchBar");
+        webSteps.click("SearchBy_SearchButton");
+        String actualResult = webSteps.getTableCellText(1, columnIndex);
+        Assert.assertEquals(actualResult.trim(), inputValue.trim(), "Search result mismatch for criteria: " + criteriaType);
     }
 
 }
